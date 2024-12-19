e47fe452996407cebb33595a154cbb46b5114d01
konradkjems <konrad@tapfeed.dk>
2024-12-19 09:23:16 +0100

    fixed npm loop 4

--- server.js
+++ server.js
@@ -84,8 +84,8 @@ if (!app) {
 
 // CORS konfiguration
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production'
-        ? ['https://my.tapfeed.dk', 'https://tapfeed.vercel.app']
+    origin: process.env.NODE_ENV === 'production' 
+        ? ['https://mytapfeed.vercel.app', 'https://my.tapfeed.dk']
         : 'http://localhost:3001',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
