b94bb823228bbe3483a7bc02a918384f67fdea39
konradkjems <konrad@tapfeed.dk>
2024-12-21 19:21:08 +0100

    v0.3.2.0 fixed landing pages preview online 3

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ if (!app) {
 // Middleware setup
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
-        ? ['https://my.tapfeed.dk', 'https://api.tapfeed.dk']
+        ? ['https://my.tapfeed.dk', 'https://api.tapfeed.dk', 'https://tapfeed.dk', /\.tapfeed\.dk$/]
         : ['http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
